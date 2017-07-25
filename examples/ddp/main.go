78b4e9ff6855e9487dcf3b904d3968c3b3d70a73
Naoki Takano <honten@tinkermode.com>
2017-07-25 13:39:01 -0700

    Removed device info

--- examples/ddp/main.go
+++ examples/ddp/main.go
@@ -11,8 +11,8 @@ import (
 
 func initDevice() {
 	dc := &mode.DeviceContext{
-		DeviceID:  1460,
-		AuthToken: "v1.ZHwxNDYw.1478030094.51d1fc621a9c40addade401a4eb5f9d9b05054c171a62035e0d0750666f2c9ec4dbe841fd1ee2c8b733b9e1d69acfffa5c3e448c7567c9c64a67870b017f289efefc11495ec9e20c",
+		DeviceID:  0,
+		AuthToken: "",
 	}
 	if err := mode.StartSession(dc, true); err != nil {
 		panic(err)
